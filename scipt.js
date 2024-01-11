0a0fd3a7625c96b676868106af224c940ca530e6
TejasNate <150812999+TejasNate@users.noreply.github.com>
2024-01-11 12:15:12 +0530

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,8 @@
+document.getElementById("class").style.backgroundColor = "yellow";
+document.getElementById("class").style.margin = "20px";
+document.getElementById("class").style.padding = "10px";
+document.getElementById("class").style.fontSize = "18px";
+document.getElementById("class").style.fontWeight = "bold";
+document.getElementById("class").style.height = "200px";
+document.getElementById("class").style.width = "300px";
+document.getElementById("class").style.color = "blue";
